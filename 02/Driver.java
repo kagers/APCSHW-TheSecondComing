3facc7a5183b23709e744ef3b380954ce337211f
Katherine Gershfeld <ka.gershfeld@gmail.com>
2015-02-11 22:35:04 -0500

    sort of tour

---
+++ 02/Driver.java
@@ -0,0 +1,17 @@
+public class Driver{
+
+    public static void main(String[]args){
+	KnightsTour k;
+	int size;
+	try{
+	    size = Integer.parseInt(args[0]);
+	} catch (Exception e){
+	    size = 4;
+	}
+	k = new KnightsTour(size);
+	System.out.println(k.clear);
+	k.solve();
+	System.out.println(k.solveable());
+    }
+
+}
---
+++ 02/KnightsTour.java
@@ -0,0 +1,94 @@
+import java.util.*;
+import java.io.*;
+
+
+public class KnightsTour{
+    //constants for the class
+    //terminal specific character to clear screen , or hide/show cursor
+    final static String clear =  "\033[2J";
+    final static String hide =  "\033[?25l";
+    final static String show =  "\033[?25h";
+    
+    //instance variable
+    private int[][]board;
+    private boolean solved = false;
+    
+    //terminal specific character to move the cursor
+    private String go(int x,int y){
+	return ("\033[" + x + ";" + y + "H");
+    }
+    
+    public void wait(int millis){
+	try {
+	    Thread.sleep(millis);
+	}
+	catch (InterruptedException e) {
+	}
+    }
+    
+    public String toString(){
+	String ans = "\n";
+	//build your knights tour here...
+	for (int i=0; i<board.length; i++){
+	    for (int j=0; j<board[i].length; j++){
+		ans+=board[i][j]+"\t";
+	    }
+	    ans+="\n\n";
+	}
+	return hide + go(0,0) + ans + "\n" + show;
+    }
+    
+    public KnightsTour(int size){
+	board = new int[size][size];
+	for (int i=0; i<size; i++){
+	    for (int j=0; j<size; j++){
+		board[i][j] = -1;
+	    }
+	}
+    }
+    
+    public void solve(){
+	solve(0,0);
+    }
+
+    public void solve(int startx, int starty){
+	solved = solve(startx, starty, 0);
+    }
+    
+    public boolean solve(int x,int y,int currentMoveNumber){
+	System.out.println(clear);
+	System.out.println(this);
+	//wait(10);
+	
+	if ((x>=board[1].length || x<0 || y>=board.length || y<0) ||
+	    (board[y][x]!=-1)){
+	    return false;
+	}
+	board[y][x] = currentMoveNumber;
+	if (currentMoveNumber==Math.pow(board.length,2)-1){
+	    return true;
+	}
+	if (!(solve(x-2,y+1,currentMoveNumber+1) ||
+	      solve(x+2,y-1,currentMoveNumber+1) ||
+	      solve(x+2,y+1,currentMoveNumber+1) ||
+	      solve(x-2,y-1,currentMoveNumber+1) ||
+	      solve(x+1,y+2,currentMoveNumber+1) ||
+	      solve(x+1,y-2,currentMoveNumber+1) ||
+	      solve(x-1,y+2,currentMoveNumber+1) ||
+	      solve(x-1,y-2,currentMoveNumber+1))){
+	    board[y][x] = -1;
+	    return false;
+	    //} else {
+	    //board[y][x] = currentMoveNumber;
+	    //return true;
+	} else{
+	    return true;
+	}
+	//return false;
+    }
+    
+    public boolean solveable(){
+	return solved;
+    }
+
+}
