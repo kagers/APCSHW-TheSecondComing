ddc35321bb7c86bca4b6e5519842c4c82ebc143f
Katherine Gershfeld <ka.gershfeld@gmail.com>
2015-03-01 21:05:01 -0500

    layk

---
+++ 04USACO/makelake.java
@@ -0,0 +1,79 @@
+public class makelake{
+    
+    public static int lake(int[] one, int[][] two, int[] three, int[] four){
+	two = directions(two, three);
+	//System.out.println("hello !");
+	two = directions(two, four);
+	int depth = one[2];
+	int adepth = 0;
+	for (int i = 0; i < two.length; i++){
+	    for (int j = 0; j < two[i].length; j++){
+		if (two[i][j] < depth){
+		    two[i][j] = depth - two[i][j];
+		}else{
+		    two[i][j] = 0;
+		}
+		adepth += two[i][j];
+	    }
+	}
+	return adepth*72*72;
+    }
+
+    public static int[] findMax(int[][] array, int startRow, int startCol, int endRow, int endCol){
+	int max = array[startRow][startCol];
+	int[] index = new int[2];
+	for (int i = startRow; i < endRow; i++){
+	    for (int j = startCol; j < endCol; j++){
+		if (array[i][j] >= max){
+		    max = array[i][j];
+		    index[0] = i;
+		    index[1] = j;
+		}
+	    }
+	}
+	return index;
+    }
+
+    public static int[][] directions(int[][] map, int[] instructions){
+	int startRow = instructions[0] - 1;
+	int startCol = instructions[1] - 1;
+	int[] index = findMax(map, startRow, startCol, startRow + 3, startCol + 3);
+	int ans = map[index[0]][index[1]] - instructions[2];
+	for (int i = startRow; i < startRow + 3; i++){
+	    for (int j = startCol; j < startCol + 3; j++){
+		if (map[i][j] > ans){
+		    map[i][j] = ans;
+		}
+	    }
+	}
+	return map;
+	
+    }
+
+    public static String printA(int[][] map){
+	String ans = "{";
+	for (int i = 0; i < map.length; i++){
+	    ans += "{";
+	    for (int j = 0; j < map[i].length; j++){
+		ans += map[i][j] + ",";
+	    }
+	    ans += "}\n";
+	}
+	ans += "}";
+	return ans;
+    }
+
+    public static void main(String[]args){
+	int[][] map = new int[][]{
+	    {28,25,20,32,34,36},
+	    {27,25,20,20,30,34},
+	    {24,20,20,20,20,30},
+	    {20,20,14,14,20,20}
+	};
+	int[] one = {4,6,22,2};
+	int[] three = {1,4,4};
+	int[] four = {1,1,10};
+
+	System.out.println(lake(one,map,three,four));
+    }
+}
