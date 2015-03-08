634d7bc13ca275694c7a7b7d10f3c06657e0edfa
Katherine Gershfeld <ka.gershfeld@gmail.com>
2015-03-08 17:55:26 -0400

    selquict

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -27,21 +27,18 @@ public class Quick{
     }
 
     public static int quickselect(int[] ary, int n){
-	return quickSelecth(ary,n,0,ary.length-1);
+	return partition(ary,n,0,ary.length-1);
     }
 
-    public static int quickSelecth(int[] ary, int n, int si, int ei){
-	//System.out.println("\n"+Arrays.toString(ary));
+    public static int partition(int[] ary, int n, int si, int ei){
 	int[] D = new int[ary.length];
 	for (int i=0;i<D.length;i++){
 	    if (i<si || i>ei){
 		D[i]=ary[i];
 	    }
 	}
-	//System.out.println(Arrays.toString(D));
 	Random ayn = new Random();
 	int pi = ayn.nextInt(ei-si+1)+si;
-	//System.out.println(ary[pi]);
 	int ie = ei;
 	int is = si;
 	for (int i=si; i<=ie; i++){
@@ -50,20 +47,48 @@ public class Quick{
 	    } else if (ary[i]!=ary[pi]){
 		D[ei--]=ary[i];
 	    }
-	    //System.out.println(Arrays.toString(D));
 	}
 	D[si]=ary[pi];
-	//System.out.println(Arrays.toString(D));
-	//System.out.println(n+" "+ei);
 	if (n<ei){
-	    return quickSelecth(D,n,is,si);
+	    return partition(D,n,is,si);
 	} else if (n>ei){
-	    return quickSelecth(D,n,ei,ie);
+	    return partition(D,n,ei,ie);
 	} else{
 	    return D[ei];
 	}
 	
-    }    
+    }
+
+    public static int ipquickselect(int[] ary, int n){
+	return ippartition(ary,n,0,ary.length-1);
+    }
+
+    public static int ippartition(int[] ary, int n, int si, int ei){
+	Random ayn = new Random();
+	int pivot = ary[ayn.nextInt(ei-si+1)+si];
+	int ie = ei;
+	int is = si;
+	while (si<ei){
+	    if (ary[si]>pivot){
+		int hi = ary[si];
+		ary[si] = ary[ei];
+		ary[ei] = hi;
+		ei--;
+	    } else if (ary[si]<pivot){
+		si++;
+	    } else{
+		ary[si]=ary[ei];
+		ary[ei]=pivot;
+	    }
+	}
+	if (n<ei){
+	    return ippartition(ary,n,is,si);
+	} else if (n>ei){
+	    return ippartition(ary,n,ei,ie);
+	} else{
+	    return ary[ei];
+	}
+    }
     
     public static void randomize(int[] ary){
 	Random rand = new Random();
@@ -77,16 +102,18 @@ public class Quick{
 
     public static void main(String[] args){
 	int[] a = {16,8,7,5,3,2,10,41,1,13,91};
-	int[] b = {0,1,2,3,4,5,6,7,8,9,10};
+	int[] b = {0,1,55,3,4,5,6,7,8,9,10};
 	Quick.randomize(b);
 	System.out.println(Arrays.toString(b));
-	/*Quick.partition(a,0,a.length-1);
+	/*Quick.ippartition(a,0,a.length-1);
 	System.out.println();
-	Quick.partition(a,1,4);*/
-	try{
+	Quick.ippartition(a,1,4);
+	*/try{
 	    System.out.println(Quick.quickselect(b,Integer.parseInt(args[0])));
+	    System.out.println(Quick.ipquickselect(b,Integer.parseInt(args[0])));
 	} catch(Exception e){
 	    System.out.println(Quick.quickselect(b,2));
+	    System.out.println(Quick.ipquickselect(b,2));
 	}
     }
 
--- other/Quick.java
+++ other/Quick.java
@@ -26,11 +26,68 @@ public class Quick{
 	System.out.println(Arrays.toString(D));
     }
 
+    public static int quickselect(int[] ary, int n){
+	return quickSelecth(ary,n,0,ary.length-1);
+    }
+
+    public static int quickSelecth(int[] ary, int n, int si, int ei){
+	System.out.println("\n"+Arrays.toString(ary));
+	int[] D = new int[ary.length];
+	for (int i=0;i<D.length;i++){
+	    if (i<si || i>ei){
+		D[i]=ary[i];
+	    }
+	}
+	System.out.println(Arrays.toString(D));
+	Random ayn = new Random();
+	int pi = ayn.nextInt(ei-si+1)+si;
+	System.out.println(ary[pi]);
+	int ie = ei;
+	int is = si;
+	for (int i=si; i<=ie; i++){
+	    if (ary[i]<ary[pi]){
+		D[si++]=ary[i];
+	    } else if (ary[i]!=ary[pi]){
+		D[ei--]=ary[i];
+	    }
+	    System.out.println(Arrays.toString(D));
+	}
+	D[si]=ary[pi];
+	System.out.println(Arrays.toString(D));
+	System.out.println(n+" "+ei);
+	if (n<ei){
+	    return quickSelecth(D,n,is,si);
+	} else if (n>ei){
+	    return quickSelecth(D,n,ei,ie);
+	} else{
+	    return D[ei];
+	}
+	
+    }    
+    
+    public static void randomize(int[] ary){
+	Random rand = new Random();
+        for (int i=0;i<ary.length;i++){
+	    int bi = rand.nextInt(ary.length);
+	    int hi = ary[i];
+	    ary[i] = ary[bi];
+	    ary[bi] = hi;
+	}
+    }
+
     public static void main(String[] args){
-	int [] a  = {16,8,7,5,3,2,10,41,1,13,91};
-	Quick.partition(a,0,a.length-1);
+	int[] a = {16,8,7,5,3,2,10,41,1,13,91};
+	int[] b = {0,1,2,3,4,5,6,7,8,9,10};
+	System.out.println(Arrays.toString(b));
+	Quick.randomize(b);
+	/*Quick.partition(a,0,a.length-1);
 	System.out.println();
-	Quick.partition(a,1,4);
+	Quick.partition(a,1,4);*/
+	try{
+	    System.out.println(Quick.quickselect(b,args[0]));
+	} catch(Exception e){
+	    System.out.println(Quick.quickselect(b,2));
+	}
     }
 
 }
