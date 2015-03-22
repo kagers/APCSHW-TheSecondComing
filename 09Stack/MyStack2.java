c264c539d48c8d387ae7efd907080e1ddd06de4a
Katherine Gershfeld <ka.gershfeld@gmail.com>
2015-03-22 15:39:15 -0400

    mystack w mylinkedlist

---
+++ 09Stack/MyStack2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class MyStack2<T>{
+    
+    private MyLinkedList<T> ll;
+
+    public MyStack2(){
+	ll = new MyLinkedList<T>();
+    }
+
+    public T push(T item){
+	ll.add(0,item);
+	return item;
+    }
+
+    public T pop(){
+	if (!empty()){
+	    peek();
+	    return ll.remove(0);
+	}
+	throw new EmptyStackException();
+    }
+
+    public T peek(){
+	if (!empty()){
+	    return ll.get(0);
+	}
+	throw new EmptyStackException();
+    }
+
+    public boolean empty(){
+	return ll.size()==0;
+    }
+
+}
