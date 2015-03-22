6db269262687f939c3f7f0408208f0adfa4275e0
Katherine Gershfeld <ka.gershfeld@gmail.com>
2015-03-21 23:17:10 -0400

    started

---
+++ 09Stack/MyStack.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class MyStack<T>{
+    
+    private LinkedList<T> stack;
+
+    public MyStack(){
+	stack = new LinkedList<T>();
+    }
+
+    public T push(T item){
+	stack.add(0,item);
+	return item;
+    }
+
+    public T peek(){
+	return stack.get(0);
+    }
+
+    public void pop(){
+	peek();
+	return remove(0);
+    }
+
+    public boolean empty(){
+	return stack.size()==0;
+    }
+
+}
