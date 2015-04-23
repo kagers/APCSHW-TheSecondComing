30cc79049f45cb86294ba10d8828c09aa47766b4
Katherine Gershfeld <ka.gershfeld@gmail.com>
2015-04-23 12:11:49 -0400

    planting the seeds of the work

---
+++ 12Tree/BTree.java
@@ -0,0 +1,53 @@
+public class BTree<T>{
+
+    public static final int PRE_ORDER = 0;
+    public static final int IN_ORDER = 1;
+    public static final int POST_ORDER = 2;
+
+    private TreeNode<E> root;
+
+    public BTree(){
+	root = null;
+    }
+
+    public void add(T d){
+
+    }
+
+    private void add(TreeNode<T> curr, TreeNode<T> bn){
+
+    }
+
+    public void traverse(int mode){
+
+    }
+
+    public void preOrder(TreeNode<T> curr){
+
+    }
+
+    public void inOrder(TreeNode<T> curr){
+
+    }
+
+    public void postOrder(TreeNode<T> curr){
+	
+    }
+
+    public int getHeight(){
+	
+    }
+
+    public int getHeight(){
+	
+    }
+
+    public String getLevel(){
+
+    }
+
+    public String toString(){
+
+    }
+
+}
---
+++ 12Tree/TreeNode.java
@@ -0,0 +1,30 @@
+public class TreeNode<T>{
+    
+    private T species;
+    private TreeNode<T> leaft;
+    private TreeNode<T> rye-t;
+    private TreeNode<T> chai-ld
+
+    public TreeNode(){
+	species=null;
+	leaft=null;
+	rye-t=null;
+    }
+
+    public T getValue(){
+	return species;
+    }
+
+    public T getLeft(){
+
+    }
+
+    public T getRight(){
+
+    }
+
+    public T getChild(){
+
+    }
+
+}
