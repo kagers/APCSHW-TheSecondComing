66e8f38f7ea5d22c06311f79e42c240c1f5f76e8
Katherine Gershfeld <ka.gershfeld@gmail.com>
2015-05-13 22:51:54 -0400

    we are 95% confident that im gonna die right now p=0.005

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class MyHeap{
 
     private int[] hepa;
@@ -16,27 +18,50 @@ public class MyHeap{
 	return "";
     }
 
+    private int getParent(int i){
+	return i/2;
+    }
+
+    private int getLeft(int i){
+	return i*2;
+    }
+
+    private int getRight(int i){
+	return i*2+1;
+    }
+
+    private void swap(int j, int k){
+	int temp = hepa[j];
+	hepa[j] = hepa[k];
+	hepa[k] = temp;
+    }
+
     public int remove(){
 	if (hepa[0]>0){
 	    int ret = hepa[1];
-	    for (int i=1; i<hepa[0]-1; i++){
-		hepa[i] = hepa[i+1];
-	    }
+	    hepa[1] = hepa[hepa[0]];
+	    hepa[0]--;
+	    int i = 0;
+	    while (
 	    return ret;
 	} else {
-	    throw new Exception();
+	    throw new NoSuchElementException();
 	}
     }
 
-    public void add(int){
+    public void add(int ){
 
+    }
+
+    private void resize(){
+	hepa = Arrays.copyOf(hepa,hepa[0]*2);
     }
     
     public int peek(){
 	if (hepa[0]>0){
 	    return hepa[1];
 	}
-	throw new Exception();
+	throw new NoSuchElementException();
     }
 
 }
