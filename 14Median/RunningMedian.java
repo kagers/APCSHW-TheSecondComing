2034a4c4f00ae2a9c581d1a7912ef1d3cab4a90d
kagers <ka.gershfeld@gmail.com>
2015-05-14 12:30:05 -0400

    talented and perverted

--- 14Median/RunningMedian.java
+++ 14Median/RunningMedian.java
@@ -11,9 +11,9 @@ public class RunningMedian{
     public double getMedian(){
 	if (min.size()<1 && max.size()<1){
 	    throw new NoSuchElementException();
-	} else if (min.size()<1){
+	} else if (min.size()<max.size()){
 	    return max.peek();
-	} else if (max.size()<1){
+	} else if (min.size()>max.size()){
 	    return min.peek();
 	} else{
 	    return (max.peek()+min.peek())/2;
@@ -21,7 +21,21 @@ public class RunningMedian{
     }
 
     public void add(int value){
+	if (value>getMedian()){
 
+	} else if (value<getMedian()){
+
+	} else{
+
+	}
+    }
+
+    public String toString(){
+	return max+" "+min;
+    }
+
+    public static void main(String[] args){
+	RunningMedian saigon = new RunningMedian();
     }
 
 }
