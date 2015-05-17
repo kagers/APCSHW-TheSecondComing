3bbca881d8befb0e6d64e7a98bede2935e475d41
Katherine Gershfeld <ka.gershfeld@gmail.com>
2015-05-17 16:08:56 -0400

    ho chih minheap

--- 14Median/RunningMedian.java
+++ 14Median/RunningMedian.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class RunningMedian{
 
     private MyHeap min;
@@ -5,10 +7,11 @@ public class RunningMedian{
 
     public RunningMedian(){
 	min = new MyHeap(false);
-	max = new myheap(true);
+	max = new MyHeap(true);
     }
 
     public double getMedian(){
+	//System.out.println(max.size()+" "+min.size());
 	if (min.size()<1 && max.size()<1){
 	    throw new NoSuchElementException();
 	} else if (min.size()<max.size()){
@@ -16,26 +19,30 @@ public class RunningMedian{
 	} else if (min.size()>max.size()){
 	    return min.peek();
 	} else{
-	    return (max.peek()+min.peek())/2;
+	    return (max.peek()+min.peek())/2.0;
 	}
     }
 
     public void add(int value){
-	if (value>getMedian()){
-	    min.add(value);
-	} else if (value<getMedian()){
+	if (min.size()==0 && max.size()==0){
 	    max.add(value);
 	} else{
-	    if (min.size()>max.size()){
+	    if (value>getMedian()){
+		min.add(value);
+	    } else if (value<getMedian()){
 		max.add(value);
 	    } else{
-		min.add(value);
+		if (min.size()>max.size()){
+		    max.add(value);
+		} else{
+		    min.add(value);
+		}
 	    }
-	}
-	if (min.size()-max.size()>1){
-	    max.add(min.remove());
-	} else if (max.size()-min.size()>1){
-	    min.add(max.remove());
+	    if (min.size()-max.size()>1){
+		max.add(min.remove());
+	    } else if (max.size()-min.size()>1){
+		min.add(max.remove());
+	    }
 	}
     }
 
@@ -52,6 +59,30 @@ public class RunningMedian{
 	System.out.println(saigon.getMedian());
 	System.out.println(saigon);
 	saigon.add(3);
+	System.out.println(saigon.getMedian());	
+	System.out.println(saigon);
+	saigon.add(0);
+	System.out.println(saigon.getMedian());	
+	System.out.println(saigon);
+	saigon.add(7);
+	System.out.println(saigon.getMedian());	
+	System.out.println(saigon);
+	saigon.add(6);
+	System.out.println(saigon.getMedian());	
+	System.out.println(saigon);
+	saigon.add(4);
+	System.out.println(saigon.getMedian());	
+	System.out.println(saigon);
+	saigon.add(1);
+	System.out.println(saigon.getMedian());	
+	System.out.println(saigon);
+	saigon.add(9);
+	System.out.println(saigon.getMedian());	
+	System.out.println(saigon);
+	saigon.add(8);
+	System.out.println(saigon.getMedian());	
+	System.out.println(saigon);
+	saigon.add(2);
 	System.out.println(saigon.getMedian());	
 	System.out.println(saigon);
     }
